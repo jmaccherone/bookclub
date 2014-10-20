efb353df5a37a222aab489c222ac27918589c55a
Jennifer Maccherone <Jennifer@Maccherone.com>
2014-10-20 17:01:27 -0400

    calendar
    added bootstrap, jquery, iron router, full calendar
    added a menu bar (although not the right one) and a calendar

---
+++ client/bookclub.js
@@ -0,0 +1,28 @@
+
+  CalEvents = new Meteor.Collection('calevents');
+  Session.setDefault('editing_calevent', null);
+  Session.setDefault('showEditEvent', false);
+ 
+
+  Router.map(function (){
+    this.route('homepage', {path: '/'});
+  });
+
+  Router.map(function() {
+    this.route('calendar', {path: '/calendar'});
+  });
+
+Template.calendar.rendered = function(){
+  $('#calendar').fullCalendar({
+    dayClick:function(date, allDay, jsEvent, view){
+
+    },
+    eventClick:function(calEvent, jsEvent, view) {
+
+    },
+    events:function(start, end, callback) {
+
+    }
+  });
+}
+
